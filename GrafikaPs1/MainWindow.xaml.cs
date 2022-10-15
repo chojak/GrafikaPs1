55de6f952492759c5807fcec79c7e82ef4999fac
chojak <chojecki2000@gmail.com>
2022-10-15 13:05:38 +0200

    szablon jakis

--- GrafikaPs1/MainWindow.xaml.cs
+++ GrafikaPs1/MainWindow.xaml.cs
@@ -18,11 +18,63 @@ namespace GrafikaPs1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public enum DrawingMode
+    {
+        Line,
+        Draw,
+        Triangle,
+        Quatrangle,
+        Elipse
+    }
     public partial class MainWindow : Window
     {
+        public static DrawingMode DrawingMode;
+        public static Color Color;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        protected void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        protected void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            Color = (Color)ColorPicker.SelectedColor;
+        }
+
+        private void LineToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            DrawingMode = DrawingMode.Line;
+        }
+
+        private void DrawToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            DrawingMode = DrawingMode.Draw;
+        }
+
+        private void TriangleToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            DrawingMode = DrawingMode.Triangle;
+        }
+
+        private void QuadrangleToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            DrawingMode = DrawingMode.Quatrangle;
+        }
+
+        private void ElipseToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            DrawingMode = DrawingMode.Elipse;
         }
     }
 }
